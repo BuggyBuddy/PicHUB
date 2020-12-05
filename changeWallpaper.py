13b75ccbb8289688bccb4ef6267f5eb3d8882952
lpzxry <71749480+lpzxry@users.noreply.github.com>
2020-12-05 16:45:09 +0800

    Update changeWallpaper.py

--- changeWallpaper.py
+++ changeWallpaper.py
@@ -1,6 +1,9 @@
 #https://www.write-bug.com/article/1580.html
 #在这里学习了SystemParametersInfo函数的相关知识
-
+#本代码需要一些模块：PIL以及pywin32
+#若有问题请自行打开cmd，输入：
+#       pip pillow
+#       pip pywin32 
 #请将目标图片的路径放进fi.txt
 #最好是绝对路径
 #如果是相对路径请确保changeWallpaper.py文件与目标图像在同一目录
