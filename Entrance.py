6a7f5e546d4e6ea6cc28cd18f478150d50b7215c
y-yu18 <71927117+y-yu18@users.noreply.github.com>
2020-12-11 16:17:17 +0800

    Update Entrance.py

--- Entrance.py
+++ Entrance.py
@@ -56,9 +56,22 @@ def classifer(firstRun):
 		initialize.initialize()
 	return inceptionv3.predict()
 
-def recommender(result_type, result_poss):
-	#添加推荐器，返回需要搜索的结果
-	return result_type[1]
+def recommender():
+	preference_dict=open_initialize.getpreference_type_list()
+        preference_poss=open_initialize.getpreference_poss()
+        max_poss=[0,0,0,0,0]
+        max_dict=['','','','','']
+        for i in range(len(preference_poss)):
+	if preference_poss[i]>min(max_poss):
+	    #print(i)
+	    c=max_poss.index(min(max_poss))
+	    #print(c)
+	    max_poss[c]=preference_poss[i]
+	    max_dict[c]=preference_dict[i]
+	    sum_poss=sum(max_poss)
+	    for i in range(1,5):
+		max_poss[i]=max_poss[i]/sum_poss
+        return max_poss,max_dict
 
 	
 
