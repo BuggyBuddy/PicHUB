0c9c0f83dea85abd930289f03239649ea4b2e6e0
Joxer5 <71872000+Joxer5@users.noreply.github.com>
2020-12-18 20:10:11 +0800

    Add files via upload

--- WPSource.py
+++ WPSource.py
@@ -1,32 +1,90 @@
 from Crawler import *
-
+import threading
+from PyQt5 import QtCore, QtGui, QtWidgets
+import inspect
+import ctypes
 '''
 使用方法：
 建立WPSource对象，构造函数输入(网址源，类型，关键词)
-	网址源目前包括{"Pixabay","Pexels","Unsplash"}
+	网址源目前包括{"Pixabay","Pexels","Unsplash", "WallPapersCraft"}
 	类型默认为temp
 
 
 通过getImageList获取WPImage图像列表
 '''
+import pichub
 
 class WPSource(object):
 	WPList = [] #壁纸列表
+	WPusedList = []
 	crawlerType = None
 	authorisation = None
 	parserElement = None
+	perPage = 9
+
 	def __init__(self, sourceWeb, genre = "temp", keyWord = None):
+		self.page = 1
 		self.sourceWeb = sourceWeb
 		self.genre = genre
 		self.keyWord = keyWord
+		self.thread = threading.Thread(target = self.fetch)
+
+	def run(self):
+		#self.stop_thread(self.thread)
+		self.thread = threading.Thread(target = self.fetch)
+		self.thread.start()
+
+	def fetch(self):
 		url = self.createURL()
 		if self.crawlerType == "API":
 			crawler = APICrawler(self.sourceWeb, url)
 			crawler.request(self.authorisation)
 			crawler.parse()
-			for x in crawler.imageList:
-				x.download(self.genre)
-		self.WPList = crawler.imageList
+		if self.crawlerType == "Auto":
+			crawler = AutoCrawler(self.sourceWeb, url)
+			crawler.request()
+			crawler.parse()
+		for x in crawler.imageList:
+			x.download(self.genre)
+			print("download")
+		self.WPList.extend(crawler.imageList) #append
+		pichub.showWallPaper()
+		print("hahaha")
+		
+
+	def nextPage(self):
+		self.page = self.page + 1
+
+	def toPage(self, page):
+		self.page = page
+
+	def changeSourceWeb(self, sourceWeb):
+		self.sourceWeb = sourceWeb
+
+	def changeKeyWord(self, keyWord):
+		self.keyWord = keyWord
+
+	def changePerPage(self, perPage):
+		self.perPage = perPage
+
+	def getImageList(self):
+		return self.WPList
+
+	def markUsed(self, name):
+		mark = next((x for x in self.WPList if x.fileName == name), None)
+		self.WPusedList.append(mark)
+		self.WPList.remove(mark)
+
+	def markLike(self, name, like):
+		for x in WPusedList:
+			if x.fileName == name:
+				x.like = True
+
+	def getLikeList(self):
+		return [x.like == True for x in self.WPusedList]
+
+	def inStock(self):
+		return self.WPList.size()
 
 	def createURL(self):
 		if self.sourceWeb == "Pixabay":
@@ -36,15 +94,15 @@ class WPSource(object):
 				"q": self.keyWord,
 				"lang": "en",
 				"image_type": "all",
-				"orientation": "all",
+				"orientation": "horizontal",
 				"category": "all",
 				"colors": "all",
 				"editors_choice": "false",
-				"order": "popular",
-				"per_page": "9",
-				"page": "1"
+				"order": "latest",
+				"per_page": str(self.perPage),
+				"page": str(self.page)
 				}
-			url = "https://pixabay.com/api/?key="+API_KEY+"&pretty=true"
+			url = "https://pixabay.com/api/?key="+API_KEY
 			for key in parameter:
 				url = url + "&" + key + "=" + parameter[key]
 			return url
@@ -56,8 +114,8 @@ class WPSource(object):
 			parameter = {
 				"query": self.keyWord,
 				"orientation": "landscape",
-				"per_page": "9",
-				"page": "1"
+				"per_page": str(self.perPage),
+				"page": str(self.page)
 			}
 			url = "https://api.pexels.com/v1/search?query=" + self.keyWord
 			for key in parameter:
@@ -70,20 +128,38 @@ class WPSource(object):
 			parameter = {
 				"query": self.keyWord,
 				"orientation": "landscape",
-				"per_page": "9",
-				"page": "1"
+				"per_page": str(self.perPage),
+				"page": str(self.page)
 			}
 			url = "https://api.unsplash.com/search/photos?client_id="+API_KEY
 			for key in parameter:
 				url = url + "&" + key + "=" + parameter[key]
 			return url
 
-	def getImageList(self):
-		return self.WPList
+		if self.sourceWeb == "WallPapersCraft":
+			self.crawlerType = "Auto"
+			url = "https://wallpaperscraft.com/search/?order=downloads&page=" + str(self.page) + "&query=" + self.keyWord
+			return url
+
+
+class WebRecommender(object):
+	def __init__(self, keyWord):
+		self.url = "https://unsplash.com/s/photos/" + keyWord
+		self.crawler = Crawler("Recommender", self.url)
+		self.crawler.request()
+		self.recommendList = self.parse()
+
+	def parse(self):
+		soup = BeautifulSoup(self.crawler.webContent, 'lxml')
+		return [x.contents[0] for x in soup.findAll(class_ = "_2634o _1CBrG _1ByhS _2bG1V xLon9")]
+
+	def getRecommendList(self):
+		return self.recommendList
 
 def main():
-	wpSource = WPSource("Unsplash", "building", "building")
-	print(wpSource.getImageList()[0].getFilePath())
+	#wpSource = WPSource("WallPapersCraft", "Animal", "wolf")
+	wr = WebRecommender("cougar, puma, catamount, mountain lion, painter, panther, Felis concolor")
+	print(wr.getRecommendList())
 
 if __name__ == "__main__":
     main()
