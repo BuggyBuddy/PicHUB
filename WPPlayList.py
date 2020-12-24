9895303219b1161e15e43101213f3bb051d60fcc
Joxer5 <71872000+Joxer5@users.noreply.github.com>
2020-12-25 03:18:08 +0800

    黄浙枫用蒋潇的电脑Commit一个测试过的版本

--- WPPlayList.py
+++ WPPlayList.py
@@ -1,56 +1,62 @@
-import random
+import threading
+import WPPlayList
 
-class WPPlayList:
+def noneFunc():
+    return
+
+class Player:
     """
-    length:壁纸队列长度
-    wallpapers:WPImage对象队列
-    showing:正被设置或者上次最后被设置的壁纸的序号
-    name:壁纸队列的名字
+    intervalTime:自动播放间隔时间（单位秒）
+    mode:播放模式
+    wallpaperList:被播放的队列
+    timer:用来实现自动播放的定时器
+    autoPlaying:自动播放的标志
     """
-    def __init__(self, name):
-        self.length = 0
-        self.wallpapers = []
-        self.showing = -1
-        self.name = name
+    intervalTime = 300
+    mode = 'InOrder'
+    wallpaerList = WPPlayList.WPPlayList("Temp")
+    timer = threading.Timer(300, noneFunc)
+    autoPlaying = 0
 
-    def setName(self, name):
-        self.name = name
+    @staticmethod
+    def setWPList(wallpaperList):
+        Player.wallpaperList = wallpaperList
 
-    def append(self, wallpaper):
-        self.wallpapers.append(wallpaper)
-        self.length = self.length + 1
+    @staticmethod
+    def setIntervalTime(time):
+        Player.intervalTime = time
 
-    def insert(self, index, wallpaper):
-        if index < 0 or index > self.length:
-            return
-        if self.showing >= index:
-            self.showing = self.showing + 1
-        self.wallpapers.insert(index, wallpaper)
-        self.length = self.length + 1
+    @staticmethod
+    def playInOrder():
+        Player.mode = 'InOrder'
+        Player.__play()
 
-    def delete(self, index):
-        if self.length == 0 or index >= self.length or index < 0:
-            return
-        if self.showing >= index:
-            self.showing = self.showing - 1
-        del self.wallpapers[index]
-        self.length = self.length - 1
+    @staticmethod
+    def playInShuffle():
+        Player.mode = 'InShuffle'
+        Player.__play()
 
-    def showIndex(self, index):
-        self.showing = index
-        self.__show(index)
-
-    def showNext(self, showingMode):
-        if showingMode == 'InOrder':
-            self.showing = (self.showing + 1) % self.length
-        elif showingMode == 'InShuffle':
-            randomIndex = self.showing
-            while randomIndex == self.showing:
-                randomIndex = random.randint(0, self.length - 1)
-            self.showing = randomIndex
+    @staticmethod
+    def playIndex(index):
+        if Player.autoPlaying:
+            Player.stop()
+            Player.wallpaperList.showIndex(index)
+            Player.__play()
         else:
-            return
-        self.__show(self.showing)
+            Player.wallpaperList.showIndex(index)
 
-    def __show(self, index):
-        self.wallpapers[index].changeWP()
+    @staticmethod
+    def stop():
+        Player.autoPlaying = 0
+        Player.timer.cancel()
+
+    @staticmethod
+    def __play():
+        timer = threading.Timer(Player.intervalTime, Player.__playNext)
+        Player.autoPlaying = 1
+        timer.start()
+
+    @staticmethod
+    def __playNext():
+        Player.wallpaperList.showNext(Player.mode)
+        Player.timer.start()
