83bea619d618b12cccff2d23701dc6256257ab12
fhl18 <70746449+fhl200008@users.noreply.github.com>
2020-12-25 09:32:42 +0800

    Update Player.py

--- Player.py
+++ Player.py
@@ -1,6 +1,11 @@
 import threading
 import WPPlayList
+import os
+from WPImage import WPImage
 
+
+listOfWPPlayList=[]
+listNum=0
 def noneFunc():
     return
 
@@ -62,3 +67,39 @@ class Player:
         Player.timer.cancel()
         Player.timer = threading.Timer(Player.intervalTime, Player.__playNext)
         Player.timer.start()
+        
+    @staticmethod
+    def __onStartUp():
+        if os.path.exists('setting.txt') and os.path.getsize('setting'):
+            with open('setting.txt','r')as file_obj:
+                listNum=file_obj.readline().rstrip()
+                for i in range(listNum):
+                    listLength=int(file_obj.readline().restrip())
+                    tempList=WPPlayList.WPPlaylist(file_obj.readline().restrip())
+                    for j in range(listLength):
+                        sourceWeb=WPPlayList.WPPlaylist(file_obj.readline().restrip())
+                        webID=WPPlayList.WPPlaylist(file_obj.readline().restrip())
+                        url=WPPlayList.WPPlaylist(file_obj.readline().restrip())
+                        width=WPPlayList.WPPlaylist(file_obj.readline().restrip())
+                        height=WPPlayList.WPPlaylist(file_obj.readline().restrip())
+                        tempImage=WPImage(sourceWeb, webID, url, width, height)
+                        templist.append(temoImage)
+                    ListOfWPPlayList.append(tempList)
+                    
+    @staticmethod
+    def __onShutDown():
+        if os.path.exists('setting.txt') :
+            with open('setting.txt','w')as file_obj:
+                file_obj.write([str(listNum),'\n'])
+                for i in range(listNum):
+                    tempList=ListOfWPPlayList[i]
+                    file_obj.write([str(tempList.len),'\n'])
+                    for j in range(tempList.len):
+                        file_obj.write([templist[j].sourceWeb,'\n'])
+                        file_obj.write([templist[j].webID,'\n'])
+                        file_obj.write([templist[j].url,'\n'])
+                        file_obj.write([templist[j].shape[0],'\n'])
+                        file_obj.write([templist[j].shape[1],'\n'])
+                        
+                        
+
